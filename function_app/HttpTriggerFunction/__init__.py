a2957e08434249e87954e57d15a22ef287095859
manoj <manojgaonkar7648@gmail.com>
2024-12-09 23:14:46 +0530

    terraform main

--- function_app/HttpTriggerFunction/__init__.py
+++ function_app/HttpTriggerFunction/__init__.py
@@ -1,22 +1,19 @@
-import logging
 import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
+    val = req.params.get('val')
+    if not val:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            name = req_body.get('val')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}!")
+    if val:
+        return func.HttpResponse(f"Function app {val}!")
     else:
         return func.HttpResponse(
-            "Please pass a name on the query string or in the request body",
+            "",
             status_code=400
         )
